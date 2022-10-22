f984a0b451738e5543966b3a118bea7e966c515d
sniph <sniph@users.noreply.github.com>
2022-10-22 07:42:34 +0200

    update item5

--- example_code/item_05.py
+++ example_code/item_05.py
@@ -16,6 +16,7 @@
 
 # Reproduce book environment
 import random
+
 random.seed(1234)
 
 import logging
@@ -37,11 +38,13 @@ OLD_CWD = os.getcwd()
 atexit.register(lambda: os.chdir(OLD_CWD))
 os.chdir(TEST_DIR.name)
 
+
 def close_open_files():
     everything = gc.get_objects()
     for obj in everything:
         if isinstance(obj, io.IOBase):
             obj.close()
+
 
 atexit.register(close_open_files)
 
@@ -49,65 +52,68 @@ atexit.register(close_open_files)
 # Example 1
 from urllib.parse import parse_qs
 
-my_values = parse_qs('red=5&blue=0&green=',
-                     keep_blank_values=True)
+my_values = parse_qs("red=5&blue=0&green=", keep_blank_values=True)
 print(repr(my_values))
 
 
 # Example 2
-print('Red:     ', my_values.get('red'))
-print('Green:   ', my_values.get('green'))
-print('Opacity: ', my_values.get('opacity'))
+print("Red:     ", my_values.get("red"))
+print("Green:   ", my_values.get("green"))
+print("Opacity: ", my_values.get("opacity"))
 
 
 # Example 3
 # For query string 'red=5&blue=0&green='
-red = my_values.get('red', [''])[0] or 0
-green = my_values.get('green', [''])[0] or 0
-opacity = my_values.get('opacity', [''])[0] or 0
-print(f'Red:     {red!r}')
-print(f'Green:   {green!r}')
-print(f'Opacity: {opacity!r}')
+red = my_values.get("red", [""])[0] or 0
+green = my_values.get("green", [""])[0] or 0
+opacity = my_values.get("opacity", [""])[0] or 0
+print(f"Red:     {red!r}")
+print(f"Green:   {green!r}")
+print(f"Opacity: {opacity!r}")
 
 
 # Example 4
-red = int(my_values.get('red', [''])[0] or 0)
-green = int(my_values.get('green', [''])[0] or 0)
-opacity = int(my_values.get('opacity', [''])[0] or 0)
-print(f'Red:     {red!r}')
-print(f'Green:   {green!r}')
-print(f'Opacity: {opacity!r}')
+red = int(my_values.get("red", [""])[0] or 0)
+green = int(my_values.get("green", [""])[0] or 0)
+opacity = int(my_values.get("opacity", [""])[0] or 0)
+print(f"Red:     {red!r}")
+print(f"Green:   {green!r}")
+print(f"Opacity: {opacity!r}")
 
 
 # Example 5
-red_str = my_values.get('red', [''])
+red_str = my_values.get("red", [""])
 red = int(red_str[0]) if red_str[0] else 0
-green_str = my_values.get('green', [''])
+green_str = my_values.get("green", [""])
 green = int(green_str[0]) if green_str[0] else 0
-opacity_str = my_values.get('opacity', [''])
+opacity_str = my_values.get("opacity", [""])
 opacity = int(opacity_str[0]) if opacity_str[0] else 0
-print(f'Red:     {red!r}')
-print(f'Green:   {green!r}')
-print(f'Opacity: {opacity!r}')
+print(red_str)
+print(green_str)
+print(opacity_str)
+
+print(f"Red:     {red!r}")
+print(f"Green:   {green!r}")
+print(f"Opacity: {opacity!r}")
 
 
 # Example 6
-green_str = my_values.get('green', [''])
+green_str = my_values.get("green", [""])
 if green_str[0]:
     green = int(green_str[0])
 else:
     green = 0
-print(f'Green:   {green!r}')
+print(f"Green:   {green!r}")
 
 
 # Example 7
 def get_first_int(values, key, default=0):
-    found = values.get(key, [''])
+    found = values.get(key, [""])
     if found[0]:
         return int(found[0])
     return default
 
 
 # Example 8
-green = get_first_int(my_values, 'green')
-print(f'Green:   {green!r}')
+green = get_first_int(my_values, "green")
+print(f"Green:   {green!r}")
