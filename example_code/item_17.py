b5616acf5c1cc09c6972cbae83defe2dad287796
sniph <sniph@users.noreply.github.com>
2022-10-29 07:26:45 +0200

    update_item17

--- example_code/item_17.py
+++ example_code/item_17.py
@@ -16,6 +16,7 @@
 
 # Reproduce book environment
 import random
+
 random.seed(1234)
 
 import logging
@@ -37,55 +38,71 @@ OLD_CWD = os.getcwd()
 atexit.register(lambda: os.chdir(OLD_CWD))
 os.chdir(TEST_DIR.name)
 
+
 def close_open_files():
     everything = gc.get_objects()
     for obj in everything:
         if isinstance(obj, io.IOBase):
             obj.close()
 
+
 atexit.register(close_open_files)
 
 
 # Example 1
 visits = {
-    'Mexico': {'Tulum', 'Puerto Vallarta'},
-    'Japan': {'Hakone'},
+    "Mexico": {"Tulum", "Puerto Vallarta"},
+    "Japan": {"Hakone"},
 }
-
+print(visits)
 
 # Example 2
-visits.setdefault('France', set()).add('Arles')  # Short
+# assign key set to key if not {} add value to set
+visits.setdefault("France", set()).add("Arles")  # Short
 
-if (japan := visits.get('Japan')) is None:       # Long
-    visits['Japan'] = japan = set()
-japan.add('Kyoto')
+# assign value to set of the key else get method defaults to "None"
+# test on "None" then assign empty set {}
+if (japan := visits.get("Japan")) is None:  # Long
+    visits["Japan"] = japan = set()
+japan.add("Kyoto")
+
+# swap print statement with pprint
 original_print = print
 print = pprint
 
 print(visits)
+pprint(visits)
+# swap print statement with pprint reverse
 print = original_print
 
 
 # Example 3
+# make class with city add method
 class Visits:
     def __init__(self):
         self.data = {}
 
     def add(self, country, city):
+        # make key,set combinations within data set
         city_set = self.data.setdefault(country, set())
         city_set.add(city)
 
 
+print(visits)
+
 # Example 4
+# initialize visits variable from  class Visits:
 visits = Visits()
-visits.add('Russia', 'Yekaterinburg')
-visits.add('Tanzania', 'Zanzibar')
+# use add method from class Visits to add cities to countries set
+visits.add("Russia", "Yekaterinburg")
+visits.add("Tanzania", "Zanzibar")
 print(visits.data)
 
 
 # Example 5
 from collections import defaultdict
 
+# default creates default value for every key so every as a value is to add to
 class Visits:
     def __init__(self):
         self.data = defaultdict(set)
@@ -93,7 +110,8 @@ class Visits:
     def add(self, country, city):
         self.data[country].add(city)
 
+
 visits = Visits()
-visits.add('England', 'Bath')
-visits.add('England', 'London')
+visits.add("England", "Bath")
+visits.add("England", "London")
 print(visits.data)
