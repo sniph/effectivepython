78fae1d179b4edbc9d817794340c09f6223e44d7
sniph <sniph@users.noreply.github.com>
2022-11-01 16:52:31 +0100

    update_item22

--- example_code/item_22.py
+++ example_code/item_22.py
@@ -16,6 +16,7 @@
 
 # Reproduce book environment
 import random
+
 random.seed(1234)
 
 import logging
@@ -37,42 +38,52 @@ OLD_CWD = os.getcwd()
 atexit.register(lambda: os.chdir(OLD_CWD))
 os.chdir(TEST_DIR.name)
 
+
 def close_open_files():
     everything = gc.get_objects()
     for obj in everything:
         if isinstance(obj, io.IOBase):
             obj.close()
 
+
 atexit.register(close_open_files)
 
 
 # Example 1
+# make log from message and join statement with tuple comprehension
+# with no values TypeError missing values
 def log(message, values):
     if not values:
         print(message)
     else:
-        values_str = ', '.join(str(x) for x in values)
-        print(f'{message}: {values_str}')
+        values_str = ", ".join(str(x) for x in values)
+        print(f"{message}: {values_str}")
 
-log('My numbers are', [1, 2])
-log('Hi there', [])
 
+log("My numbers are", [1, 2])
+log("Hi there", [])
+log("Hi there2")
 
 # Example 2
+# use " * " before variable to declare variable optional
+# no values gives "()" => not () evaluates to true
 def log(message, *values):  # The only difference
     if not values:
         print(message)
+        print(values)
     else:
-        values_str = ', '.join(str(x) for x in values)
-        print(f'{message}: {values_str}')
+        values_str = ", ".join(str(x) for x in values)
+        print(f"{message}: {values_str}")
 
-log('My numbers are', 1, 2)
-log('Hi there')  # Much better
+
+log("My numbers are", 1, 2)
+log("Hi there")  # Much better
 
 
 # Example 3
+# receive list as seperate items after message not as 1 list
 favorites = [7, 33, 99]
-log('Favorite colors', *favorites)
+log("Favorite colors", *favorites)
 
 
 # Example 4
@@ -80,21 +91,35 @@ def my_generator():
     for i in range(10):
         yield i
 
+
 def my_func(*args):
     print(args)
 
+
+# with unpack you need a variable and *variable
+# this you assign generator to a variable it and print *it
+it = my_generator()
+print(*it)
+
+# here unpack first then print a,it
+a, *it = my_generator()
+print([a, it])
+# print trough function double *variable
 it = my_generator()
 my_func(*it)
 
 
 # Example 5
 def log(sequence, message, *values):
+    # when values is empty tuple () => not () is True them no values printed
     if not values:
-        print(f'{sequence} - {message}')
+        print(f"{sequence} - {message}")
+    # if values print all
     else:
-        values_str = ', '.join(str(x) for x in values)
-        print(f'{sequence} - {message}: {values_str}')
+        values_str = ", ".join(str(x) for x in values)
+        print(f"{sequence} - {message}: {values_str}")
 
-log(1, 'Favorites', 7, 33)      # New with *args OK
-log(1, 'Hi there')              # New message only OK
-log('Favorite numbers', 7, 33)  # Old usage breaks
+
+log(1, "Favorites", 7, 33)  # New with *args OK
+log(1, "Hi there")  # New message only OK
+log("Favorite numbers", 7, 33)  # Old usage breaks
