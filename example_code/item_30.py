682a75533cf28282547008aac0ef80a80a0d32c0
sniph <sniph@users.noreply.github.com>
2022-11-07 12:41:01 +0100

    update_item30

--- example_code/item_30.py
+++ example_code/item_30.py
@@ -16,6 +16,7 @@
 
 # Reproduce book environment
 import random
+
 random.seed(1234)
 
 import logging
@@ -29,13 +30,16 @@ import io
 import os
 import tempfile
 
+# here are the example files stored
 TEST_DIR = tempfile.TemporaryDirectory()
 atexit.register(TEST_DIR.cleanup)
 
 # Make sure Windows processes exit cleanly
 OLD_CWD = os.getcwd()
 atexit.register(lambda: os.chdir(OLD_CWD))
+# Change the current working directory to the specified path.
 os.chdir(TEST_DIR.name)
+
 
 def close_open_files():
     everything = gc.get_objects()
@@ -43,71 +47,101 @@ def close_open_files():
         if isinstance(obj, io.IOBase):
             obj.close()
 
+
 atexit.register(close_open_files)
 
 
 # Example 1
+# enumerate generate index,item pair use generator like list
+# "result" looks for index number of " " characters in address string
 def index_words(text):
     result = []
+    # if there is text append first item append(0) then index + 1
     if text:
         result.append(0)
     for index, letter in enumerate(text):
-        if letter == ' ':
+        if letter == " ":
             result.append(index + 1)
     return result
 
 
 # Example 2
-address = 'Four score and seven years ago...'
-address = 'Four score and seven years ago our fathers brought forth on this continent a new nation, conceived in liberty, and dedicated to the proposition that all men are created equal.'
+address = "Four score and seven years ago..."
+address = "Four score and seven years ago our fathers brought forth on this continent a new nation, conceived in liberty, and dedicated to the proposition that all men are created equal."
+# generate index/value pairs list
+a = list(enumerate(address))
+print(a)
+
 result = index_words(address)
+# print all
+print(result)
+# first 10 reults
 print(result[:10])
 
 
 # Example 3
+# if text then append text 0 to list else if letter = " " append index + 1
+# "next" statement returns following value
 def index_words_iter(text):
     if text:
         yield 0
     for index, letter in enumerate(text):
-        if letter == ' ':
+        if letter == " ":
             yield index + 1
 
 
 # Example 4
+# "it" object to use for iterator next/lsit method
 it = index_words_iter(address)
 print(next(it))
 print(next(it))
 
 
 # Example 5
+# "list" method generates list over the enumerate yields for every item in address
 result = list(index_words_iter(address))
 print(result[:10])
 
 
 # Example 6
+
+
 def index_file(handle):
+    # initialize the offset of the file
     offset = 0
     for line in handle:
+        # if there is a line true
         if line:
+            # return value offset
             yield offset
+        # loop over lines
         for letter in line:
+            # for every letter and ! to "offset" variable
             offset += 1
-            if letter == ' ':
+            # if letter is space then return offset number
+            if letter == " ":
                 yield offset
 
 
 # Example 7
-address_lines = """Four score and seven years
+address_lines = """Four  score and seven years
 ago our fathers brought forth on this
 continent a new nation, conceived in liberty,
 and dedicated to the proposition that all men
 are created equal."""
-
-with open('address.txt', 'w') as f:
+# here are the example files stored
+# TEST_DIR = tempfile.TemporaryDirectory()
+#'C:\\Users\\HARRYS~1\\AppData\\Local\\Temp\\2\\tmphvfh2fyp'>
+with open("address.txt", "w") as f:
     f.write(address_lines)
 
 import itertools
-with open('address.txt', 'r') as f:
+
+# read the file to f:
+with open("address.txt", "r") as f:
+    # assign data to "it" variable pass through index_file function
     it = index_file(f)
+    # get the first 10 items from "it" variable with islice
     results = itertools.islice(it, 0, 10)
+    # use "list" method to generate a list of results object
     print(list(results))
